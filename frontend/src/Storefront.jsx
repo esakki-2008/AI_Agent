10087bf476405e38da3848d3da7bee93b664a9ce
ESAKKI RAJA SALAIKUMAR <esakkiraja2528@gmail.com>
2026-09-14 22:14:36 +0530

    Add ClipForge storefront wrapper

---
+++ frontend/src/Storefront.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "./App";
+import PaymentCTA from "./PaymentCTA";
+
+export default function Storefront() {
+  return (
+    <>
+      <App />
+      <PaymentCTA />
+    </>
+  );
+}
