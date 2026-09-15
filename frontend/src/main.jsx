26750a872204019c5213ce07a6d2c49a5a0bd99b
ESAKKI RAJA SALAIKUMAR <esakkiraja2528@gmail.com>
2026-09-15 15:53:12 +0530

    Show Gumroad purchase CTA in ClipForge AI storefront

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import Storefront from './Storefront'
 import './style.css'
+import './payment.css'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode><Storefront /></React.StrictMode>
 )
